35d2b42f7f061b260ac5b7e42e7d040a9932e945
Anton <antongorynya1@gmail.com>
2023-09-21 14:03:00 +0300

    Добавил аллергены из бд

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from foods.models import Dish
+from foods.models import Dish, FoodIntolerance
 
 def index(request):
     dishes = Dish.objects.filter(is_active=True)[:3]
@@ -13,4 +13,6 @@ def contacts(request):
 
 
 def order(request):
-    return render(request, 'shop/order.html', context={})
+    foods_intolerances = FoodIntolerance.objects.all()
+    print(request.GET)
+    return render(request, 'shop/order.html', context={'foods_intolerances': foods_intolerances})
