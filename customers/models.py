42348261a1a2bd3d49b9d72cea1a52669f2ac8be
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-09-22 22:39:45 +0300

    Add customer model

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Customer(User):
+    is_subscriber = models.BooleanField(
+        verbose_name='подписчик',
+        default=True
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='дата создания'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='дата обновления'
+    )
+
+    class Meta:
+        verbose_name = "клиент"
+        verbose_name_plural = "клиенты"
+
+    def __str__(self):
+        return self.username
