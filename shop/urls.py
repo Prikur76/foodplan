0bb7079e77c6e62d4f4244d8df32de382eb473f6
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-09-25 14:41:56 +0300

    Change path name 'lk' on 'account'

--- shop/urls.py
+++ shop/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('auth/', views.auth, name='auth'),
     path('registration/', views.registration, name='registration'),
-    path('lk/', views.cabinet, name='lk'),
+    path('account/', views.account, name='account'),
 ]
