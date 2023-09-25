7a704f92e08cf8892fdeef6e803e540811fe703a
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-09-25 18:20:28 +0300

    Add path "logout" (shop)

--- shop/urls.py
+++ shop/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('auth/', views.auth, name='auth'),
     path('registration/', views.registration, name='registration'),
     path('account/', views.account, name='account'),
+    path('logout/', views.logout, name='logout'),
 ]
